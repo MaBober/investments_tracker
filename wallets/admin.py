13b16d36bdde0e80a9cd9255afce94223132bffc
Marcin Bober <marcin@bober.pl>
2024-04-19 10:26:57 +0200

    Refactor admin.py and models/asset.py to seperate buying and selling actions.
    See: Create API endpoints for transaction. #11

--- wallets/admin.py
+++ wallets/admin.py
@@ -4,7 +4,7 @@ from .models import Country, Currency
 from .models import Wallet, Transaction, Deposit, Withdrawal
 from .models import Account, AccountInstitutionType, AccountInstitution, AccountType
 from .models import AssetType, MarketAsset, MarketShare, MarketETF, AssetPrice, ExchangeMarket, AssetTypeAssociation
-from .models import Deposit
+from .models import Deposit, UserAsset
 
 # Register your models here.
 
@@ -13,7 +13,7 @@ admin.site.register([Wallet])
 admin.site.register([Account, AccountInstitution, AccountInstitutionType, AccountType, ExchangeMarket])
 admin.site.register([AssetType , MarketShare, MarketETF, AssetPrice, AssetTypeAssociation])
 admin.site.register([Deposit])
-admin.site.register([Withdrawal])
+admin.site.register([Withdrawal, UserAsset])
 
 @admin.register(Transaction)
 class Transaction(admin.ModelAdmin):
