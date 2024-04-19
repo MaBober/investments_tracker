47313c506a746fe595f3bb38ba678003b69230bc
Marcin Bober <marcin@bober.pl>
2024-04-19 15:56:29 +0200

    Refactor code to add Transaction model and API endpoints.

--- wallets/serializers/__init__.py
+++ wallets/serializers/__init__.py
@@ -3,3 +3,4 @@ from .user import UserSerializer
 from .wallet import WalletSerializer, WalletCreateSerializer
 from .account import AccountSerializer, AccountCreateSerializer
 from .deposit import DepositSerializer, DepositCreateSerializer
+from .transaction import TransactionSerializer, TransactionCreateSerializer
